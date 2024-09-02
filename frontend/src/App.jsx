9692f3046777221c8264c388c3404cbac064ccf8
Your Name <your.email@example.com>
2024-09-02 20:50:23 +0530

    add footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Navbar from './components/Navbar/Navbar/Navbar'
+import Cart from './pages/cart/Cart'
+import placeOrder from './pages/placeOrder/placeOrder'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
+    <>
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element ={<Home/>} />
+        <Route path='/Cart' element={<Cart/>}/>
+        <Route path='/order' element={<placeOrder/>}/>
+
+      </Routes>
+
       
     </div>
+    <Footer/>
+    </>
   )
 }
 
